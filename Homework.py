30989c9525de31158977baf479bad11025e37831
mameenomar <ameen.omar786@gmail.com>
2014-07-30 19:20:36 +0200

    Homework .py
    Homework for first chapter of Programming for Absolute Beginners

---
+++ Homework.py
@@ -0,0 +1,25 @@
+first = "Mohamed Ameen"
+last = "Omar"
+age = 17
+print first, last, age
+
+
+import math
+
+sideA = 12.55
+sideB = 17.85
+
+sideC = math.sqrt(sideA * sideA + sideB * sideB)
+print sideC
+
+
+operand1 = 95
+operand2 = 64.5
+
+print operand1 + operand2
+print operand1 - operand2
+print operand1 * operand2
+print operand1 / operand2
+print operand1 % operand2
+
+# Could not understand what was required for last question concerning how much of the code must be re-written...
